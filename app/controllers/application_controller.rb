60839371aac1bf7df5e46b24db5d7902e019c4a2
Ezra Skolnik <ezraskolnik@gmail.com>
2022-03-14 16:45:39 -0400

    node version check for deploy user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,6 +40,7 @@ class ApplicationController < ActionController::Base
     # Use custom JS JWT library because Ruby's just doesn't work
     logger.info "node app/javascript/verify_jwt.js #{token} #{secret}"
     logger.info "RUNNING COMMAND AS #{`whoami`}"
+    logger.info "can I use node? #{`node -v`}"
 
     token_json = `node app/javascript/verify_jwt.js #{token} #{secret}`
     verify = JSON.parse(token_json)
