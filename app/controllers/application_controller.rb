12d84489b6862586e163323b36aa0565dacd3931
Ezra Skolnik <ezraskolnik@gmail.com>
2022-03-15 19:55:40 -0400

    skip verify step

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 require "jwt"
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
 
   before_action :snake_case_params
 
--- app/controllers/grimoires_controller.rb
+++ app/controllers/grimoires_controller.rb
@@ -1,5 +1,5 @@
 class GrimoiresController < ApplicationController
-  protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
   before_action :restrict_to_development, :only => [:index]
 
   # GET /grimoires
