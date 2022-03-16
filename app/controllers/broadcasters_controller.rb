51034a71166ff0d05cbef5dfa5a3fddf6ae6cf13
Ezra Skolnik <ezraskolnik@gmail.com>
2022-03-15 20:09:08 -0400

    fix multiple csrf

--- app/controllers/broadcasters_controller.rb
+++ app/controllers/broadcasters_controller.rb
@@ -1,5 +1,5 @@
 class BroadcastersController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
   before_action :restrict_to_development, :only => [:index]
 
--- app/controllers/game_sessions_controller.rb
+++ app/controllers/game_sessions_controller.rb
@@ -1,5 +1,5 @@
 class GameSessionsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
   before_action :restrict_to_development, :only => [:index]
 
