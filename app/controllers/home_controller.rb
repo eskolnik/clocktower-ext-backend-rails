c6412fdf10143ec073e8d36a85f4fafb1a6fe083
Ezra Skolnik <ezraskolnik@gmail.com>
2022-04-11 23:51:39 -0400

    redirect setup to google doc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    redirect_to "https://docs.google.com/document/d/e/2PACX-1vQ3Am2-eAzgzBX-CRZc-S2l9q1-OyCv0lv9Tw_NjF8fBUhbQ5bA0ivIUOwVADsZtudZ2qynZirKC2Ii/pub", allow_other_host: true
   end
 end
